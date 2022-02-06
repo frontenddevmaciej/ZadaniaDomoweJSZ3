f792f8014ccc52cb62401c107265829f3585bd24
frontenddevmaciej <frontenddevmaciej@gmail.com>
2022-02-06 15:21:35 +0100

    Zrobione zadanie 9

---
+++ main.js
@@ -0,0 +1,30 @@
+// >-----------------------------------------------------------------------------------------------------------------------------------------<
+
+// ZADANIA Z ZAJĘĆ
+
+// >-----------------------------------------------------------------------------------------------------------------------------------------<
+
+// Zadanie 9
+
+// Napisz skrypt, który bez użycia pętli obliczy sumę oraz iloczyn liczb naturalnych w przedziale od 1 do 10.
+
+// var tablicaLiczb = new Array(10).fill(0).map(function(value, index){
+//     return index +1;
+// });
+
+// var suma = 0;
+// var iloczyn =1;
+
+// tablicaLiczb.forEach(function sumowanie(element){
+//     suma += element;    
+// });
+
+// tablicaLiczb.forEach(function mnozenie(element){
+//     iloczyn *= element;
+// });
+
+// console.log(suma);
+// console.log(iloczyn);
+
+// >-----------------------------------------------------------------------------------------------------------------------------------------<
+
